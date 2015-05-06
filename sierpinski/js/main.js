ba5664f9be1daf56936c1ef95e348c53f85db174
CuteLifeBot <cutelifebot@gmail.com>
2015-05-06 14:36:47 -0400

    Added position tracker

--- sierpinski/js/main.js
+++ sierpinski/js/main.js
@@ -8,7 +8,8 @@ GAME.begin = function() {
     GAME.gaskets = [
         new Geometry.Sierpinski([[205,405],[405,105],[605,505]], GAME.sierpLimit)
     ];
-
+    GAME.points = 0;
+    GAME.mousepoint = [GAME.size/2, GAME.size/2];
 }
 
 GAME.update = function(dt) {
@@ -26,6 +27,14 @@ GAME.redraw = function() {
     for(var k=0; k < GAME.gaskets.length; k++) {
         GAME.gaskets[k].draw(GAME.canvas);
     }
+    GAME._drawCursor_();
+}
+
+
+
+GAME._drawCursor_ = function() {
+    GAME.canvas.line(GAME.mousepoint[0], GAME.mousepoint[1]-5, GAME.mousepoint[0], GAME.mousepoint[1]+5).stroke({color: '#700', width: 2});
+    GAME.canvas.line(GAME.mousepoint[0]-5, GAME.mousepoint[1], GAME.mousepoint[0]+5, GAME.mousepoint[1]).stroke({color: '#700', width: 2});
 }
 
 
@@ -34,8 +43,14 @@ $(document).ready(function() {
     var scale = 0.9; // Canvas will a square that is 90% the width of the window's smallest dimension
     var gridWidthHeight = 0.9*Math.min($(document).width(), $(document).height());
     GAME.canvas = SVG('canvas').size(gridWidthHeight, gridWidthHeight);
+    GAME.size = gridWidthHeight;
+    GAME.canvasPos = getPosition(GAME.canvas.node);
     GAME.timestamp = null;
     GAME.dt = 30; // milliseconds
+
+    GAME.canvas.mousemove(function(e) {
+        GAME.mousepoint = [e.clientX-GAME.canvasPos[0], e.clientY-GAME.canvasPos[1]];
+    });
 
     // Main loop of the game, updates approximately once every GAME.dt milliseconds
     GAME.mainloop = function(timestamp) {
@@ -85,5 +100,17 @@ $(document).ready(function() {
     /*
      * WARNING - MAGIC ABOVE
      */
+
+    function getPosition(element) {
+        var xPosition = 0;
+        var yPosition = 0;
+
+        while(element) {
+            xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+            yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+            element = element.offsetParent;
+        }
+        return [xPosition, yPosition];
+    }
 
 });
